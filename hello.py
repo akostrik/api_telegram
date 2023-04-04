edb2596e0c8322255ec81ed2f8af8e14ca15710c
akostrik <internet124816@gmail.com>
2023-04-04 16:51:37 +0200

    get messages of a chat

--- hello.py
+++ hello.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python
-print "Hello World!!!"
+import asyncio
+from telethon import TelegramClient
+
+api_id = 356016
+api_hash = 'a8eb9553bc3789a544de55cc91912716'
+client = TelegramClient('anon', api_id, api_hash)
+
+async def func():
+	me = await client.get_me()
+	print(me.username)
+	print(me.phone)
+	#await client.send_message('Totokop', 'Hello')
+	async for message in client.iter_messages('+7(969)717-70-31'):
+		print(message.id, message.text)
+with client:
+	client.loop.run_until_complete(func())
