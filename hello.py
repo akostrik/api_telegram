5bd2556af32ec4ee5edf12820af78d6a378cfe2b
ann <akostrik@e3r5p1.clusters.42paris.fr>
2023-04-14 20:28:46 +0200

    connection mangoDB

--- hello.py
+++ hello.py
@@ -4,14 +4,26 @@ from telethon import TelegramClient
 
 api_id = 356016
 api_hash = 'a8eb9553bc3789a544de55cc91912716'
-client = TelegramClient('anon', api_id, api_hash)
+clientTG = TelegramClient('anon', api_id, api_hash)
+#channel = 'stalin_gulag'
+group = 'blues34'
 
 async def func():
-	me = await client.get_me()
-	print(me.username)
-	print(me.phone)
-	#await client.send_message('Totokop', 'Hello')
-	async for message in client.iter_messages('+7(969)717-70-31'):
+	me = await clientTG.get_me()
+	print('I am : ' + me.username + ' ' + me.phone)
+	await clientTG.send_message(group, 'Hello from python')
+	async for message in clientTG.iter_messages(group):
 		print(message.id, message.text)
-with client:
-	client.loop.run_until_complete(func())
+with clientTG:
+	clientTG.loop.run_until_complete(func())
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://anna:1AQ2ZS3ED@cluster0.fnbrrzu.mongodb.net/?retryWrites=true&w=majority"
+clientDB = MongoClient(uri, server_api=ServerApi('1'))
+try:
+    clientDB.admin.command('ping')
+    print("You successfully connected to MongoDB")
+except Exception as e:
+    print(e)
+
