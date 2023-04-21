4cff684570f1e1d02fa2a691f5bcf9ef25d44115
ann <akostrik@e3r10p2.clusters.42paris.fr>
2023-04-21 23:08:38 +0200

    litte changes

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+# перед выполнением войти в аккаунт на https://cloud.mongodb.com/v2/64307490cb24c97c9638b0c4#/clusters и нажать "add current ip qdresse"
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 uri = "mongodb+srv://anna:1AQ2ZS3ED@cluster0.fnbrrzu.mongodb.net/?retryWrites=true&w=majority"
@@ -11,23 +12,22 @@ try:
 except Exception as e:
     print(e)
 
+# messagesDB = clientDB["messsagesBD"]
+# collections = messagesDB.list_collection_names()
+# print('\ncollections : ')
+# print(collections)
 
-messagesDB = clientDB["messsagesBD"]
-collections = messagesDB.list_collection_names()
-print('\ncollections : ')
-print(collections)
+# collection1 = messagesDB.collection1
+# print('\ncollection : ')
+# print(collection1)
 
-collection1 = messagesDB.collection1
-print('\ncollection : ')
-print(collection1)
-
-message = {
-"text": "text message 1",
-"date": "2023-04-14",
-}
-id = collection1.insert_one(message).inserted_id
-print('\nid inserted message : ')
-print(id)
+#message = {
+#"text": "text message 1",	
+#"date": "2023-04-14",
+#}
+#id = collection1.insert_one(message).inserted_id
+#print('\nid inserted message : ')
+#print(id)
 
 # print('\ncollection.find_one : ')
 # print(collection.find_one({"Created": "collection1"}))
